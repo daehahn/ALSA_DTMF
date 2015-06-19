829e7755a051ed1f0770e0e101783c1dfb59bea6
Kyle Poore <kylepoore1@gmail.com>
2015-06-19 17:32:48 -0400

    reader takes stdin

--- Reader.cpp
+++ Reader.cpp
@@ -18,15 +18,14 @@ void Reader::run()
 	if (!pool) return;
 	for (size_t i = 0; i < num_items; ++i)
 	{
-//		char input;
-//		cin >> input;
-//		char first_half = (input & 0xF0) >> 4;
-//		char second_half = (input & 0x0F);
+		unsigned char input = getchar();
+		unsigned char first_half = (input & 0xF0) >> 4;
+		unsigned char second_half = (input & 0x0F);
 		
 		short * data = new short[TONE_BUFFER_SIZE];
-		
-                //Common::copy_tone_buffer((first_half/4)%4, first_half%4, data);
-                Common::copy_tone_buffer((i/4)%4, i%4, data);
+	  cout << "sending: " << Common::code_to_char[CODE_TO_CHAR(first_half/4,first_half%4)] << endl;
+                Common::copy_tone_buffer((first_half/4), first_half%4, data);
+                //Common::copy_tone_buffer((i/4)%4, i%4, data);
 		for(int j = NUM_MARK_SAMPLES * CHANNELS; 
 					j < TONE_BUFFER_SIZE; j++) {
                   data[i] = 0;
@@ -34,14 +33,15 @@ void Reader::run()
 
 		pool->push(data);
 
-//                data = new short[TONE_BUFFER_SIZE];
-//
-//                Common::copy_tone_buffer((second_half/4)%4, second_half%4, data);
-//		for(int i = NUM_MARK_SAMPLES * CHANNELS; 
-//					i < TONE_BUFFER_SIZE; i++) {
-//                  data[i] = 0;
-//                }
-//
-//		pool->push(data);
+                data = new short[TONE_BUFFER_SIZE];
+
+	  cout << "sending: " << Common::code_to_char[CODE_TO_CHAR(second_half/4,second_half%4)] << endl;
+                Common::copy_tone_buffer((second_half/4), second_half%4, data);
+		for(int i = NUM_MARK_SAMPLES * CHANNELS; 
+				i < TONE_BUFFER_SIZE; i++) {
+                 data[i] = 0;
+               }
+
+		pool->push(data);
 	}
 }
